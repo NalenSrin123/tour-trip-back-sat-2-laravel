ad5aa630cc642c3ba5524809c5e8f0801b14f82c
Cheavchhorng27 <cheacheavchhorng@gmail.com>
2026-08-27 00:07:20 +0700

    Create migration and model of booking history and review

---
+++ database/migrations/2026_08_26_164834_create_booking_reviews_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_reviews', function (Blueprint $table) {
+            $table->id('booking_review_id');
+            $table->foreignId('booking_id')->constrained()->cascadeOnDelete();
+            $table->tinyInteger('rating');
+            $table->text('review_text')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_reviews');
+    }
+};
