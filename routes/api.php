006921dab3c504df608d8507018cc1189f91909c
Eng Tangk <geotangk@gmail.com>
2026-09-05 15:18:42 +0700

    feat: implement user creation and update API endpoints with request validation and tests

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,33 @@
 <?php
 
-use App\Http\Controllers\TourController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\DestinationController;
 use App\Http\Controllers\Api\DestinationController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\DestinationController as WebDestinationController;
+use App\Http\Controllers\TourController;
+use App\Http\Controllers\UserController as ListUserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
+// Auth
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Tours
 Route::get('/tours', [TourController::class, 'index']);
 Route::post('/tours', [TourController::class, 'store']);
 Route::put('/tours/{id}', [TourController::class, 'update']);
 Route::delete('/tours/{id}', [TourController::class, 'destroy']);
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-Route::apiResource('destinations', DestinationController::class)
-    ->only(['update', 'destroy']);
-use App\Http\Controllers\Api\DestinationController;
+
+// Destinations
 Route::get('/destinations', [DestinationController::class, 'index']);
 Route::post('/destinations', [DestinationController::class, 'store']);
+Route::apiResource('destinations', WebDestinationController::class)->only(['update', 'destroy']);
+
+// Users
+Route::get('/users', [ListUserController::class, 'index']);
+Route::prefix('users')->group(function () {
+    Route::post('/', [UserController::class, 'store']);
+    Route::put('/{user}', [UserController::class, 'update']);
+    Route::patch('/{user}', [UserController::class, 'update']);
+});
